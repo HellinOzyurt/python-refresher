6c962b2b4741d9830a2a43d0204173faf656778a
Hellin Özyurt <hellinozyurt@gmail.com>
2026-08-14 23:26:58 +0300

    methods

---
+++ numbers-strings.py
@@ -0,0 +1,19 @@
+# NUMBERS AND STRINGS
+# a = 19
+# a = 'Hello AI ERA'
+# a = 9.2
+
+# k = type(9.2)
+# print(k)
+
+# print("a" + "b")
+
+#METHODS
+# a = "hello"
+# print(len(a))
+
+# print(a.upper())
+# print(a.lower())
+
+# print(a.islower())
+
